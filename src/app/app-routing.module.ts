1a3db6945b4912a330bc8ac1835fe925a697e82a
Swapnil Patel <p.swap1210@gmail.com>
2023-01-03 23:53:35 -0600

    frozen static data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,72 +4,68 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NotFoundComponent } from './error/not-found/not-found.component';
 import { AuthGuard } from './app-routing.guard';
-import { AuthService } from './services/auth.service';
 import { LoginComponent } from './login/login.component';
 import { Role } from './models/role';
 import { CommonService } from './services/common.service';
 import {
-	AngularFireAuthGuard,
-	hasCustomClaim,
-	redirectUnauthorizedTo,
-	redirectLoggedInTo,
-	canActivate,
-	AuthPipe,
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+  AuthPipe,
 } from '@angular/fire/compat/auth-guard';
 import { map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 const adminOnly: AuthPipe = map((user) => {
-	if (user && user.uid) {
-		//check in firestore if this uid exists
-		console.log('user.uid', user.uid);
-		return user.uid == 'WCWBz46VZQfgEsrB3niPv0Id0mD2';
-	} else {
-		return false;
-	}
+  if (user && user.uid) {
+    //check in firestore if this uid exists
+    console.log('user.uid', user.uid);
+    return user.uid == 'WCWBz46VZQfgEsrB3niPv0Id0mD2';
+  } else {
+    return false;
+  }
 });
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 const redirectLoggedInToProfile = () => redirectLoggedInTo(['profile']);
 
 const routes: Routes = [
-	{
-		path: '',
-		children: [
-			{
-				path: '',
-				component: HomeComponent,
-			},
-			{
-				path: 'profile',
-				component: ProfileComponent,
-				canLoad: [AuthGuard],
-				canActivate: [AuthGuard],
-			},
-			{
-				path: 'login',
-				component: LoginComponent,
-			},
-		],
-	},
-	{
-		path: 'admin',
-		canLoad: [AuthGuard],
-		canActivate: [AuthGuard],
-		data: {
-			roles: [Role.Admin],
-		},
-		loadChildren: () =>
-			import('./admin/admin.module').then((m) => m.AdminModule),
-	},
-	{
-		path: '**',
-		component: NotFoundComponent,
-	},
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
+    data: {
+      roles: [Role.Admin],
+    },
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
-	providers: [AuthGuard, CommonService],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard, CommonService],
 })
 export class AppRoutingModule {}
--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -33,23 +33,92 @@ export class CommonService {
   }
 
   initBasicInfo = () => {
-    let self = this;
-    this.afs
-      .doc<any>(`common/basic-info`)
-      .get()
-      .subscribe({
-        next: (val) => {
-          console.log('comm basic', val.data());
-          self.header$.next(val.data().header);
-          self.body$.next(val.data().body);
-        },
-        complete: () => {
-          console.log('Done looking for common data');
-        },
-      });
+    this.header$.next(this.basicInfoObj.header);
+    this.body$.next(this.basicInfoObj.footer);
+    // let self = this;
+    // this.afs
+    //   .doc<any>(`common/basic-info`)
+    //   .get()
+    //   .subscribe({
+    //     next: (val) => {
+    //       console.log('comm basic', val.data());
+    //       self.header$.next(val.data().header);
+    //       self.body$.next(val.data().body);
+    //     },
+    //     complete: () => {
+    //       console.log('Done looking for common data');
+    //     },
+    //   });
   };
 
   toggleTheme = (mode: boolean) => {
     this.darkThemeState$.next(mode);
+  };
+
+  basicInfoObj = {
+    header: {
+      appIconLink: 'https://avatars.githubusercontent.com/u/16183749?v=4',
+      appName: 'Swapnil Patel',
+      github: {
+        logo: 'M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z',
+        link: 'https://github.com/swap1210',
+      },
+      appDesc: '[ Full Stack Developer | Software Architect ]',
+      linkedin: {
+        logo: 'M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z',
+        link: 'https://www.linkedin.com/in/swap1210/',
+      },
+    },
+    body: {
+      notification: [
+        {
+          message: 'Hi this just happened too',
+          title: 'News2',
+          date: {
+            seconds: 1660688466,
+            nanoseconds: 539000000,
+          },
+        },
+        {
+          title: 'News1',
+          date: {
+            seconds: 1660691777,
+            nanoseconds: 445000000,
+          },
+          message: 'Hi this just happened',
+        },
+        {
+          title: 'Hi',
+          message: 'Hello there',
+        },
+      ],
+      welcome: {
+        title: 'Hello there!',
+        message:
+          "Here you can type in some welcome message to give an initial impression about the site for first times. This won't be displayed the second time.",
+      },
+      important: {
+        title: 'Caution:',
+        underConstruction: true,
+        message: 'This site is under construction. Expect bugs.',
+      },
+    },
+    footer: [
+      {
+        icon: 'https://avatars.githubusercontent.com/u/16183749?v=4',
+        title: 'Terms and Condition',
+        link: 'tnc',
+      },
+      {
+        link: 'about-us',
+        title: 'About us',
+        icon: 'https://avatars.githubusercontent.com/u/16183749?v=4',
+      },
+      {
+        icon: 'https://avatars.githubusercontent.com/u/16183749?v=4',
+        title: 'Contact',
+        link: 'contact',
+      },
+    ],
   };
 }
